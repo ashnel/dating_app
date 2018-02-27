01dba2d80946a751b94b7c35b686beecd9c1411d
ashnel <nelmaj@mail.broward.edu>
2018-02-27 15:12:53 -0800

    fix routes/redirects and add settings page to update user info

--- apps/login_app/urls.py
+++ apps/login_app/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.homepage),
-    url(r'^profile', views.personal_profile, name="my_submit"),
+    url(r'^dashboard', views.dashboard, name="my_submit"),
+    url(r'logout$', views.logout, name='my_logout'),
+    url(r'settings$', views.settings, name='my_settings'),
+    url(r'^update$', views.update, name="my_update"),
 ]
